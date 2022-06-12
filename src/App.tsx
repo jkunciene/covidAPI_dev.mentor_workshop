d8fb6e108507284629c99ea8055321a4eda0c099
jkunciene <j.kunciene@gmail.com>
2022-06-12 12:02:49 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "components/Header";
+import MainRouter from "navigation/MainRouter";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainRouter />
+    </>
+  );
+}
+
+export default App;
